cb3c251592d72c39a1b94794b391973f3a371fc2
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-16 12:06:11 +0000

    feat: added register method with StoreRequest rules

--- laravel/app/Http/Controllers/AuthController.php
+++ laravel/app/Http/Controllers/AuthController.php
@@ -2,9 +2,26 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Requests\StoreUserRequest;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Tymon\JWTAuth\Facades\JWTAuth;
 
 class AuthController extends Controller
 {
-    //
+    public function register(StoreUserRequest $request)
+    {
+        $user = User::create([
+            'name'     => $request->name,
+            'email'    => $request->email,
+            'password' => $request->password,
+        ]);
+ 
+        $token = JWTAuth::fromUser($user);
+ 
+        return response()->json([
+            'message' => 'User registered successfully.',
+            'token'   => $token,
+        ], 201);
+    }
 }
