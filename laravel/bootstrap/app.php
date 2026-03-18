4e8edc26db1e2f5bd19f39526da064efe103bf7b
Achraf-podcast <achrafagourram6@gmail.com>
2026-03-18 11:15:30 +0000

    feat: added Admin middelware

--- laravel/bootstrap/app.php
+++ laravel/bootstrap/app.php
@@ -14,6 +14,7 @@ return Application::configure(basePath: dirname(__DIR__))
     ->withMiddleware(function (Middleware $middleware): void {
         $middleware->alias([
             'employee' => \App\Http\Middleware\IsEmployee::class,
+            'admin'    => \App\Http\Middleware\IsAdmin::class,
         ]);
     })
     ->withExceptions(function (Exceptions $exceptions): void {
